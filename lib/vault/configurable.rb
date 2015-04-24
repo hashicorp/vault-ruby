ea7dbda5e2e61c67e3b4cf5eb2582e9efad5d344
Seth Vargo <sethvargo@gmail.com>
2015-04-24 15:45:41 -0400

    Create configurations and default values

---
+++ lib/vault/configurable.rb
@@ -0,0 +1,48 @@
+require_relative "defaults"
+
+module Vault
+  module Configurable
+    def self.keys
+      @keys ||= [
+        :endpoint,
+        :token,
+        :proxy_address,
+        :proxy_password,
+        :proxy_port,
+        :proxy_username,
+        :ssl_pem_file,
+        :ssl_verify,
+      ]
+    end
+
+    Vault::Configurable.keys.each(&method(:attr_accessor))
+
+    # Configure yields self for block-style configuration.
+    #
+    # @yield [self]
+    def configure
+      yield self
+    end
+
+    # Reset all the values to their defaults.
+    #
+    # @return [self]
+    def reset!
+      defaults = Defaults.options
+      Vault::Configurable.keys.each do |key|
+        instance_variable_set(:"@#{key}", defaults[key])
+      end
+      self
+    end
+    alias_method :setup!, :reset!
+
+    # The list of options for this configurable.
+    #
+    # @return [Hash<Symbol, Object>]
+    def options
+      Hash[*Vault::Configurable.keys.map do |key|
+        [key, instance_variable_get(:"@#{key}")]
+      end.flatten]
+    end
+  end
+end
---
+++ lib/vault/defaults.rb
@@ -0,0 +1,68 @@
+module Vault
+  module Defaults
+    # The default vault endpoint.
+    # @return [String]
+    VAULT_ENDPOINT = "https://127.0.0.1:8200".freeze
+
+    class << self
+      # The list of calculated options for this configurable.
+      # @return [Hash]
+      def options
+        Hash[*Configurable.keys.map { |key| [key, public_send(key)] }.flatten]
+      end
+
+      # The endpoint to communicate with Vault.
+      # @return [String]
+      def endpoint
+        ENV["VAULT_ADDR"] || VAULT_ENDPOINT
+      end
+
+      # The vault token to use for authentiation.
+      # @return [String, nil]
+      def token
+        ENV["VAULT_TOKEN"]
+      end
+
+      # The HTTP Proxy server address as a string
+      # @return [String, nil]
+      def proxy_address
+        ENV["VAULT_PROXY_ADDRESS"]
+      end
+
+      # The HTTP Proxy server username as a string
+      # @return [String, nil]
+      def proxy_username
+        ENV["VAULT_PROXY_USERNAME"]
+      end
+
+      # The HTTP Proxy user password as a string
+      # @return [String, nil]
+      def proxy_password
+        ENV["VAULT_PROXY_PASSWORD"]
+      end
+
+      # The HTTP Proxy server port as a string
+      # @return [String, nil]
+      def proxy_port
+        ENV["VAULT_PROXY_PORT"]
+      end
+
+      # The path to a pem on disk to use with custom SSL verification
+      # @return [String, nil]
+      def ssl_pem_file
+        ENV["VAULT_SSL_CERT"]
+      end
+
+      # Verify SSL requests (default: true)
+      #
+      # @return [true, false]
+      def ssl_verify
+        if ENV["VAULT_SSL_VERIFY"].nil?
+          true
+        else
+          %w[t y].include?(ENV["VAULT_SSL_VERIFY"].downcase[0])
+        end
+      end
+    end
+  end
+end
