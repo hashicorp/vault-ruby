c2702fb0380385b82b6daceb4b935b0f029aa181
Seth Vargo <sethvargo@gmail.com>
2015-04-24 19:13:36 -0400

    Custom output for responses

--- lib/vault/response.rb
+++ lib/vault/response.rb
@@ -5,6 +5,15 @@ module Vault
         def self.decode(object)
           self.new(*object.values_at(*self.members))
         end
+
+        def to_s
+          "#<#{self.class.name}>"
+        end
+
+        def inspect
+          data = self.members.map { |m| "@#{m}=#{self.public_send(m).inspect}" }.join(", ")
+          "#<#{self.class.name} #{data}>"
+        end
       end
     end
   end
