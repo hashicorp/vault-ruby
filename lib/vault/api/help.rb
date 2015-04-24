0eb560e9db42254a0a0f48beca2a41fef053a9c6
Seth Vargo <sethvargo@gmail.com>
2015-04-24 15:46:31 -0400

    Add endpoints for help

---
+++ lib/vault/api/help.rb
@@ -0,0 +1,23 @@
+require_relative "../client"
+require_relative "../response"
+
+module Vault
+  # Help is the response from a help query.
+  class Help < Response.new(:help, :see_also); end
+
+  class Client
+    # Gets help for the given path.
+    #
+    # @example
+    #   Vault.help #=> #<Vault::Help help="..." see_also="...">
+    #
+    # @param [String] path
+    #   the path to get help for
+    #
+    # @return [Help]
+    def help(path)
+      json = self.get("/v1/#{path}", help: 1)
+      return Help.decode(json)
+    end
+  end
+end
