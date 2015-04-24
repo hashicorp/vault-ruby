8500a650c05da87d778acafc6aaae5fc3d3b6a68
Seth Vargo <sethvargo@gmail.com>
2015-04-24 19:00:30 -0400

    Add specs for leader

---
+++ spec/integration/api/sys/leader_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+module Vault
+  describe Sys do
+    subject { vault_test_client.sys }
+
+    describe "#leader" do
+      it "returns leader information" do
+        result = subject.leader
+        expect(result).to be_a(LeaderStatus)
+        expect(result.ha_enabled).to be(false)
+        expect(result.ha_enabled?).to be(false)
+        expect(result.ha?).to be(false)
+        expect(result.is_self?).to be(false)
+        expect(result.is_leader?).to be(false)
+        expect(result.leader?).to be(false)
+        expect(result.address).to eq("")
+      end
+    end
+  end
+end
