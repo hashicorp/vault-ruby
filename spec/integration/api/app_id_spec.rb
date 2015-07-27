0462d7b537580e224cd0f64c3c30671568864e8c
Dylan Canfield <Dylan.Canfield@colorado.edu>
2015-07-27 16:31:08 -0600

    Changed syntax on calls to logical.write for spec testing setup

--- spec/integration/api/app_id_spec.rb
+++ spec/integration/api/app_id_spec.rb
@@ -7,8 +7,8 @@ $USER_ID = "bar"
 vault_test_client.sys.enable_auth('app-id', 'app-id', nil)
 
 # Configure App ID and User ID assocaited with the root policy
-vault_test_client.logical.write("auth/app-id/map/app-id/#{$APP_ID}", "value":"root", "display_name":$APP_ID)
-vault_test_client.logical.write("auth/app-id/map/user-id/#{$USER_ID}", "value":$APP_ID)
+vault_test_client.logical.write("auth/app-id/map/app-id/#{$APP_ID}", {"value" => "root", "display_name" => $APP_ID})
+vault_test_client.logical.write("auth/app-id/map/user-id/#{$USER_ID}", {"value" => $APP_ID})
 
 module Vault
   describe AppId do
