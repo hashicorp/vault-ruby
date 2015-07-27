ec262a909188995287894bb34d4944f0ffdc484a
Dylan Canfield <Dylan.Canfield@colorado.edu>
2015-07-27 16:11:53 -0600

    Fixing syntax to pass travis-ci build

--- spec/integration/api/app_id_spec.rb
+++ spec/integration/api/app_id_spec.rb
@@ -7,8 +7,8 @@ $USER_ID = "bar"
 vault_test_client.sys.enable_auth('app-id', 'app-id', nil)
 
 # Configure App ID and User ID assocaited with the root policy
-vault_test_client.logical.write("auth/app-id/map/app-id/#{$APP_ID}", {"value":"root", "display_name":$APP_ID})
-vault_test_client.logical.write("auth/app-id/map/user-id/#{$USER_ID}", {"value":$APP_ID})
+vault_test_client.logical.write("auth/app-id/map/app-id/#{$APP_ID}", "value":"root", "display_name":$APP_ID)
+vault_test_client.logical.write("auth/app-id/map/user-id/#{$USER_ID}", "value":$APP_ID)
 
 module Vault
   describe AppId do
@@ -16,7 +16,7 @@ module Vault
 
     describe "#login" do
       it "creates a new client token" do
-        result = subject.login({"app_id":$APP_ID, "user_id":$USER_ID})
+        result = subject.login("app_id":$APP_ID, "user_id":$USER_ID)
         expect(result).to be_a(Vault::Secret)
         expect(result.auth).to be_a(Vault::SecretAuth)
         expect(result.auth.client_token).to be
