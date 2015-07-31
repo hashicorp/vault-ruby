159e33e62f3364ed2f43095fcdf9777395deb06a
Seth Vargo <sethvargo@gmail.com>
2015-07-31 11:02:39 -0400

    Add new auth endpoints for token and app_id

---
+++ spec/integration/api/auth_spec.rb
@@ -0,0 +1,55 @@
+require "spec_helper"
+
+module Vault
+  describe Auth do
+    subject { vault_test_client }
+
+    describe "#token" do
+      it "verifies the token and saves it on the client" do
+        subject.auth.token(subject.token)
+        expect(subject.token).to eq(subject.token)
+      end
+
+      it "raises an error if the token is invalid" do
+        expect {
+          expect {
+            subject.auth.token("nope-not-real")
+          }.to raise_error(HTTPError)
+        }.to_not change(subject, :token)
+      end
+    end
+
+    describe "#app_id" do
+      before(:context) do
+        @app_id  = "aeece56e-3f9b-40c3-8f85-781d3e9a8f68"
+        @user_id = "3b87be76-95cf-493a-a61b-7d5fc70870ad"
+
+        vault_test_client.sys.enable_auth("app-id", "app-id", nil)
+        vault_test_client.logical.write("auth/app-id/map/app-id/#{@app_id}", { value: "root" })
+        vault_test_client.logical.write("auth/app-id/map/user-id/#{@user_id}", { value: @app_id })
+
+        vault_test_client.sys.enable_auth("new-app-id", "app-id", nil)
+        vault_test_client.logical.write("auth/new-app-id/map/app-id/#{@app_id}", { value: "root" })
+        vault_test_client.logical.write("auth/new-app-id/map/user-id/#{@user_id}", { value: @app_id })
+      end
+
+      it "authenticates and saves the token on the client" do
+        result = subject.auth.app_id(@app_id, @user_id)
+        expect(subject.token).to eq(result.auth.client_token)
+      end
+
+      it "authenticates with custom options" do
+        result = subject.auth.app_id(@app_id, @user_id, mount: "new-app-id")
+        expect(subject.token).to eq(result.auth.client_token)
+      end
+
+      it "raises an error if the authentication is bad" do
+        expect {
+          expect {
+            subject.auth.app_id("nope", "bad")
+          }.to raise_error(HTTPError)
+        }.to_not change(subject, :token)
+      end
+    end
+  end
+end
