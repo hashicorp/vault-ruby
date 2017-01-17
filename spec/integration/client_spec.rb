bea38ad5931da7aa64ae2c91358b8e5c740f4aaa
Seth Vargo <sethvargo@gmail.com>
2017-01-17 13:22:50 -0500

    Faster test

--- spec/integration/client_spec.rb
+++ spec/integration/client_spec.rb
@@ -11,19 +11,25 @@ module Vault
     end
 
     describe "#request" do
-      it "raises HTTPConnectionError if it takes too long to read packets from the connection" do
+      it "raises HTTPConnectionError if it takes too long to read packets from the connection", :focus do
         TCPServer.open('localhost', 0) do |server|
           Thread.new do
-            client = server.accept
-            sleep 3
-            client.close
+            loop do
+              client = server.accept
+              sleep 0.25
+              client.close
+            end
           end
 
           address = "http://%s:%s" % ["localhost", server.addr[1]]
 
           client = described_class.new(address: address, token: "foo", read_timeout: 0.01)
 
-          expect { client.request(:get, "/", {}, {}) }.to raise_error(HTTPConnectionError)
+          expect {
+            client.request(:get, "/", {}, {})
+          }.to raise_error(HTTPConnectionError)
+
+          server.close
         end
       end
 
