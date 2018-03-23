ccffc6284fa09bd3f1df8709a61518f7e2e71a99
tcosgrave <tcosgrave@wayfair.com>
2018-03-23 17:02:41 +0100

    Add shudown funciton to client
    This change exposes the persistent pool shutdown fuction to
    the vault client to help manage sockets kept open by long
    running proccesses.

--- spec/integration/client_spec.rb
+++ spec/integration/client_spec.rb
@@ -52,5 +52,65 @@ module Vault
         }.to raise_error(MissingTokenError)
       end
     end
+
+    describe "#shutdown" do
+      it "clears the pool after calling shutdown and sets nhp to nil" do
+        TCPServer.open('localhost', 0) do |server|
+          Thread.new do
+            loop do
+              client = server.accept
+              sleep 0.25
+              client.close
+            end
+          end
+
+          address = "http://%s:%s" % ["localhost", server.addr[1]]
+
+          client = described_class.new(address: address, token: "foo")
+
+          expect { client.request(:get, "/", {}, {}) }.to raise_error(HTTPConnectionError)
+
+          pool = client.instance_variable_get(:@nhp).pool
+
+          client.shutdown()
+
+          expect(pool.available.instance_variable_get(:@enqueued)).to eq(0)
+          expect(pool.available.instance_variable_get(:@shutdown_block)).not_to be_nil
+          expect(client.instance_variable_get(:@nhp)).to be_nil
+
+          server.close
+        end
+      end
+
+      it "the pool is recreated on the following request" do
+        TCPServer.open('localhost', 0) do |server|
+          Thread.new do
+            loop do
+              client = server.accept
+              sleep 0.25
+              client.close
+            end
+          end
+
+          address = "http://%s:%s" % ["localhost", server.addr[1]]
+
+          client = described_class.new(address: address, token: "foo")
+
+          expect { client.request(:get, "/", {}, {}) }.to raise_error(HTTPConnectionError)
+
+          client.shutdown()
+
+          expect { client.request(:get, "/", {}, {}) }.to raise_error(HTTPConnectionError)
+
+          pool = client.instance_variable_get(:@nhp).pool
+
+          expect(pool.available.instance_variable_get(:@enqueued)).to eq(1)
+          expect(pool.available.instance_variable_get(:@shutdown_block)).to be_nil
+          expect(client.instance_variable_get(:@nhp)).not_to be_nil
+
+          server.close
+        end
+      end
+    end
   end
 end
