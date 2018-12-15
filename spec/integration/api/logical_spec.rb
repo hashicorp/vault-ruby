695c12fe8fb262b066dd9da57f1dc5502dccb8a4
Pat Allan <pat@freelancing-gods.com>
2018-12-15 21:02:02 +1100

    Ensure secret is an unversioned KV store when we need it to be.
    In old versions of Vault (pre-0.10) this is fine, nothing needs to happen. In newer versions, temporarily replace the versioned KV with an unversioned KV instead.

--- spec/integration/api/logical_spec.rb
+++ spec/integration/api/logical_spec.rb
@@ -5,24 +5,32 @@ module Vault
     subject { vault_test_client.logical }
 
     before(:context) do
+      next unless versioned_kv_by_default?
+
+      vault_test_client.sys.unmount("secret")
       vault_test_client.sys.mount(
-        "unversioned", "kv", "v1 KV", options: {version: "1"}
+        "secret", "kv", "v1 KV", options: {version: "1"}
       )
     end
 
     after(:context) do
-      vault_test_client.sys.unmount("unversioned")
+      next unless versioned_kv_by_default?
+
+      vault_test_client.sys.unmount("secret")
+      vault_test_client.sys.mount(
+        "secret", "kv", "v2 KV", options: {version: "2"}
+      )
     end
 
     describe "#list" do
       it "returns the empty array when no items exist" do
-        expect(subject.list("unversioned/that/never/existed")).to eq([])
+        expect(subject.list("secret/that/never/existed")).to eq([])
       end
 
       it "returns all secrets" do
-        subject.write("unversioned/test-list-1", foo: "bar")
-        subject.write("unversioned/test-list-2", foo: "bar")
-        secrets = subject.list("unversioned")
+        subject.write("secret/test-list-1", foo: "bar")
+        subject.write("secret/test-list-2", foo: "bar")
+        secrets = subject.list("secret")
         expect(secrets).to be_a(Array)
         expect(secrets).to include("test-list-1")
         expect(secrets).to include("test-list-2")
@@ -31,19 +39,19 @@ module Vault
 
     describe "#read" do
       it "returns nil with the thing does not exist" do
-        expect(subject.read("unversioned/foo/bar/zip")).to be(nil)
+        expect(subject.read("secret/foo/bar/zip")).to be(nil)
       end
 
       it "returns the secret when it exists" do
-        subject.write("unversioned/test-read", foo: "bar")
-        secret = subject.read("unversioned/test-read")
+        subject.write("secret/test-read", foo: "bar")
+        secret = subject.read("secret/test-read")
         expect(secret).to be
         expect(secret.data).to eq(foo: "bar")
       end
 
       it "allows special characters" do
-        subject.write("unversioned/b:@c%n-read", foo: "bar")
-        secret = subject.read("unversioned/b:@c%n-read")
+        subject.write("secret/b:@c%n-read", foo: "bar")
+        secret = subject.read("secret/b:@c%n-read")
         expect(secret).to be
         expect(secret.data).to eq(foo: "bar")
       end
@@ -51,32 +59,32 @@ module Vault
 
     describe "#write" do
       it "creates and returns the secret" do
-        subject.write("unversioned/test-write", zip: "zap")
-        result = subject.read("unversioned/test-write")
+        subject.write("secret/test-write", zip: "zap")
+        result = subject.read("secret/test-write")
         expect(result).to be
         expect(result.data).to eq(zip: "zap")
       end
 
       it "overwrites existing secrets" do
-        subject.write("unversioned/test-overwrite", zip: "zap")
-        subject.write("unversioned/test-overwrite", bacon: true)
-        result = subject.read("unversioned/test-overwrite")
+        subject.write("secret/test-overwrite", zip: "zap")
+        subject.write("secret/test-overwrite", bacon: true)
+        result = subject.read("secret/test-overwrite")
         expect(result).to be
         expect(result.data).to eq(bacon: true)
       end
 
       it "allows special characters" do
-        subject.write("unversioned/b:@c%n-write", foo: "bar")
-        subject.write("unversioned/b:@c%n-write", bacon: true)
-        secret = subject.read("unversioned/b:@c%n-write")
+        subject.write("secret/b:@c%n-write", foo: "bar")
+        subject.write("secret/b:@c%n-write", bacon: true)
+        secret = subject.read("secret/b:@c%n-write")
         expect(secret).to be
         expect(secret.data).to eq(bacon: true)
       end
 
       it "respects spaces properly" do
-        key = 'unversioned/sub/"Test Group"'
+        key = 'secret/sub/"Test Group"'
         subject.write(key, foo: "bar")
-        expect(subject.list("unversioned/sub")).to eq(['"Test Group"'])
+        expect(subject.list("secret/sub")).to eq(['"Test Group"'])
         secret = subject.read(key)
         expect(secret).to be
         expect(secret.data).to eq(foo:"bar")
@@ -85,22 +93,22 @@ module Vault
 
     describe "#delete" do
       it "deletes the secret" do
-        subject.write("unversioned/delete", foo: "bar")
-        expect(subject.delete("unversioned/delete")).to be(true)
-        expect(subject.read("unversioned/delete")).to be(nil)
+        subject.write("secret/delete", foo: "bar")
+        expect(subject.delete("secret/delete")).to be(true)
+        expect(subject.read("secret/delete")).to be(nil)
       end
 
       it "allows special characters" do
-        subject.write("unversioned/b:@c%n-delete", foo: "bar")
-        expect(subject.delete("unversioned/b:@c%n-delete")).to be(true)
-        expect(subject.read("unversioned/b:@c%n-delete")).to be(nil)
+        subject.write("secret/b:@c%n-delete", foo: "bar")
+        expect(subject.delete("secret/b:@c%n-delete")).to be(true)
+        expect(subject.read("secret/b:@c%n-delete")).to be(nil)
       end
 
       it "does not error if the secret does not exist" do
         expect {
-          subject.delete("unversioned/delete")
-          subject.delete("unversioned/delete")
-          subject.delete("unversioned/delete")
+          subject.delete("secret/delete")
+          subject.delete("secret/delete")
+          subject.delete("secret/delete")
         }.to_not raise_error
       end
     end
@@ -114,7 +122,7 @@ module Vault
         expect(unwrapped.auth.client_token).to be
 
         vault_test_client.with_token(unwrapped.auth.client_token) do |client|
-          expect { client.logical.read("unversioned/test") }.to_not raise_error
+          expect { client.logical.read("secret/test") }.to_not raise_error
         end
       end
     end
@@ -127,7 +135,7 @@ module Vault
         expect(unwrapped).to be
 
         vault_test_client.with_token(unwrapped) do |client|
-          expect { client.logical.read("unversioned/test") }.to_not raise_error
+          expect { client.logical.read("secret/test") }.to_not raise_error
         end
       end
 
@@ -138,7 +146,7 @@ module Vault
         expect(unwrapped).to be
 
         vault_test_client.with_token(unwrapped) do |client|
-          expect { client.logical.read("unversioned/test") }.to_not raise_error
+          expect { client.logical.read("secret/test") }.to_not raise_error
         end
       end
 
