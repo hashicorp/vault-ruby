9c799feb9c700d279e94c77e78ba4445079feeab
Pat Allan <pat@freelancing-gods.com>
2018-12-15 20:25:26 +1100

    KV interface for versioned approach.

--- lib/vault/api.rb
+++ lib/vault/api.rb
@@ -5,6 +5,7 @@ module Vault
     require_relative "api/auth_tls"
     require_relative "api/auth"
     require_relative "api/help"
+    require_relative "api/kv"
     require_relative "api/logical"
     require_relative "api/secret"
     require_relative "api/sys"
