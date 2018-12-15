18e0561e5dfe51dc00cd7cd319b158db50d67eb6
Pat Allan <pat@freelancing-gods.com>
2018-12-15 20:33:09 +1100

    Don't use auto-mounted KV store for logical/redirect tests.
    In recent versions of Vault, this KV mounted at /secret is versioned, but in old versions it isn't. To keep behaviour consistent, mount an unversioned KV store for these tests instead.

--- spec/integration/redirection_spec.rb
+++ spec/integration/redirection_spec.rb
@@ -11,20 +11,30 @@ module Vault
       RSpec::RedirectServer.start
     end
 
+    before(:context) do
+      vault_test_client.sys.mount(
+        "redirection", "kv", "v1 KV", options: {version: "1"}
+      )
+    end
+
+    after(:context) do
+      vault_test_client.sys.unmount("redirection")
+    end
+
     describe "#request" do
       it "handles redirections properly in GET requests" do
         expect(redirected_client.get("/v1/sys/policy")[:policies]).to include('root')
       end
 
       it "handles redirections properly in PUT requests" do
-        redirected_client.put("/v1/secret/redirect", { works: true }.to_json)
-        expect(vault_test_client.logical.read('secret/redirect').data[:works]).to eq(true)
+        redirected_client.put("/v1/redirection/redirect", { works: true }.to_json)
+        expect(vault_test_client.logical.read('redirection/redirect').data[:works]).to eq(true)
       end
 
       it "handles redirections properly in DELETE requests" do
-        vault_test_client.logical.write('secret/redirect', { deleted: false })
-        redirected_client.delete("/v1/secret/redirect")
-        expect(vault_test_client.logical.read('secret/redirect')).to be_nil
+        vault_test_client.logical.write('redirection/redirect', { deleted: false })
+        redirected_client.delete("/v1/redirection/redirect")
+        expect(vault_test_client.logical.read('redirection/redirect')).to be_nil
       end
 
       it "handles redirections properly in POST requests" do
