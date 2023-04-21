314ff90ffb55e494ee75d73bc386f7f8f4727f5a
Daniel Huckins <dhuckins@users.noreply.github.com>
2023-04-21 16:45:03 -0400

    remove app id
    Signed-off-by: Daniel Huckins <dhuckins@users.noreply.github.com>

--- spec/integration/api/auth_spec.rb
+++ spec/integration/api/auth_spec.rb
@@ -25,43 +25,6 @@ module Vault
       end
     end
 
-    describe "#app_id" do
-      before(:context) do
-        @app_id  = "aeece56e-3f9b-40c3-8f85-781d3e9a8f68"
-        @user_id = "3b87be76-95cf-493a-a61b-7d5fc70870ad"
-
-        vault_test_client.sys.enable_auth("app-id", "app-id", nil)
-        vault_test_client.logical.write("auth/app-id/map/app-id/#{@app_id}", { value: "default" })
-        vault_test_client.logical.write("auth/app-id/map/user-id/#{@user_id}", { value: @app_id })
-
-        vault_test_client.sys.enable_auth("new-app-id", "app-id", nil)
-        vault_test_client.logical.write("auth/new-app-id/map/app-id/#{@app_id}", { value: "default" })
-        vault_test_client.logical.write("auth/new-app-id/map/user-id/#{@user_id}", { value: @app_id })
-      end
-
-      before do
-        subject.token = nil
-      end
-
-      it "authenticates and saves the token on the client" do
-        result = subject.auth.app_id(@app_id, @user_id)
-        expect(subject.token).to eq(result.auth.client_token)
-      end
-
-      it "authenticates with custom options" do
-        result = subject.auth.app_id(@app_id, @user_id, mount: "new-app-id")
-        expect(subject.token).to eq(result.auth.client_token)
-      end
-
-      it "raises an error if the authentication is bad" do
-        expect {
-          expect {
-            subject.auth.app_id("nope", "bad")
-          }.to raise_error(HTTPError)
-        }.to_not change(subject, :token)
-      end
-    end
-
     describe "#approle", vault: ">= 0.6.1" do
       before(:context) do
         @approle  = "sample-role-name"
